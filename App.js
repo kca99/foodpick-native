7c0aee78a5e2e738b5c27a4fa307b516f0c71822
Fluffisaurus <angushon96@gmail.com>
2018-07-12 22:24:30 -0700

    tidied up styles and design

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { TouchableHighlight, Button, View, Text, StyleSheet } from 'react-native
 import { createStackNavigator } from 'react-navigation';
 
 var type = "poop"
-var colors = ["powderblue", "white", "lightyellow", "lightblue"];
+var colors = ["lightgrey", "lightgrey", "lightgrey", "lightgrey"];
 var category = ['East Asian', 'European', 'South East Asian', 'Other'];
 
 var array = [];
@@ -14,7 +14,7 @@ class ParentCuisine extends React.Component {
     super(props);
     this.state = {
       item: 'East Asian',
-      tab1: 'grey',
+      tab1: 'skyblue',
       tab2: colors[1],
       tab3: colors[2],
       tab4: colors[3],
@@ -28,7 +28,7 @@ class ParentCuisine extends React.Component {
       //console.log("Entering 1");
       this.setState({
         item: thing,
-        tab1: 'grey',
+        tab1: 'skyblue',
         tab2: colors[1],
         tab3: colors[2],
         tab4: colors[3],
@@ -43,7 +43,7 @@ class ParentCuisine extends React.Component {
       this.setState({
         item: thing,
         tab1: colors[0],
-        tab2: 'grey',
+        tab2: 'skyblue',
         tab3: colors[2],
         tab4: colors[3]
       }, () => {
@@ -57,7 +57,7 @@ class ParentCuisine extends React.Component {
         item: thing,
         tab1: colors[0],
         tab2: colors[1],
-        tab3: 'grey',
+        tab3: 'skyblue',
         tab4: colors[3]
       }, () => {
         //console.log('new cuisine', this.state.item);
@@ -72,7 +72,7 @@ class ParentCuisine extends React.Component {
         tab1: colors[0],
         tab2: colors[1],
         tab3: colors[2],
-        tab4: 'grey'
+        tab4: 'skyblue'
       }, () => {
         //console.log('new cuisine', this.state.item);
         this.props.onSelectCuisine(this.state.item);
@@ -83,25 +83,25 @@ class ParentCuisine extends React.Component {
     return (
       <View style={{ flex: 1, flexDirection: 'column' }}>
         <TouchableHighlight onPress={() => this.onPress('East Asian')} underlayColor='grey'
-          style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: this.state.tab1, borderRightWidth: this.props.border }}>
+          style={[styles.buttonCategory, {backgroundColor: this.state.tab1}]}>
           <Text style={styles.categoryText}>
             {category[0]}
           </Text>
         </TouchableHighlight>
         <TouchableHighlight onPress={() => this.onPress('European')} underlayColor='grey'
-          style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: this.state.tab2, borderRightWidth: this.props.border }}>
+          style={[styles.buttonCategory, {backgroundColor: this.state.tab2}]}>
           <Text style={styles.categoryText}>
             {category[1]}
           </Text>
         </TouchableHighlight>
         <TouchableHighlight onPress={() => this.onPress('South East Asian')} underlayColor='grey'
-          style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: this.state.tab3, borderRightWidth: this.props.border }}>
+          style={[styles.buttonCategory, {backgroundColor: this.state.tab3}]}>
           <Text style={styles.categoryText}>
             {category[2]}
           </Text>
         </TouchableHighlight>
         <TouchableHighlight onPress={() => this.onPress('Other')} underlayColor='grey'
-          style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: this.state.tab4, borderRightWidth: this.props.border }}>
+          style={[styles.buttonCategory, {backgroundColor: this.state.tab4}]}>
           <Text style={styles.categoryText}>
             {category[3]}
           </Text>
@@ -118,7 +118,8 @@ class CuisineChoice extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      color: 'white'
+      color: 'white',
+      backgroundColor: 'white'
     }
   }
 
@@ -131,34 +132,37 @@ class CuisineChoice extends React.Component {
         n--;
         found = 1;
         //change state.color back to white
-
+        this.setState({
+          myButtonColor: 'transparent',
+          myButtonTextColor: 'black'
+        })
       }
     }
     if (found === 0) {
       array[n] = this.props.text;
       n++;
       //change state.color to lightyellow
-
+      this.setState({
+        myButtonColor: 'blue',
+        myButtonTextColor: 'white'
+      })
     }
     console.log(array);
   }
   render() {
     if (this.props.hidden === 'true') {
       return (
-        <View style={{
-          flex: 1, backgroundColor: 'grey',
-          margin: 10, borderRadius: 20
-        }} />
+        <View style={[styles.buttonTypeOfFood, {backgroundColor: this.state.myButtonColor}]} />
       )
     }
     else {
       return (
-        <TouchableHighlight underlayColor='grey' onPress={() => this.onPress(this.props.text)} style={{
-          flex: 1, backgroundColor: this.state.color,
-          margin: 10, borderRadius: 20, alignItems: 'center', justifyContent: 'center'
-        }}>
-          <Text style={styles.optionText}> {this.props.text} </Text>
-        </TouchableHighlight>
+        <View style={[styles.buttonTypeOfFood, {backgroundColor: this.state.backgroundColor}]}>
+          <TouchableHighlight underlayColor='lightblue' onPress={() => this.onPress(this.props.text)}
+          style={[styles.buttonTypeOfFood, {backgroundColor: this.state.myButtonColor}]}>
+            <Text style={[styles.optionText, {color: this.state.myButtonTextColor}]}> {this.props.text} </Text>
+          </TouchableHighlight>
+        </View>
       )
     }
   }
@@ -181,12 +185,12 @@ class Rightside extends React.Component {
     if (this.props.item == "East Asian") {
       return (
         <View style={{ flex: 2, backgroundColor: 'skyblue', flexDirection: 'row' }}>
-          <View style={{ flex: 1, backgroundColor: 'grey', flexDirection: 'column' }}>
+          <View style={styles.vertColRightside}>
             <CuisineChoice onPress = {this.onChange('EastAsia',0)} text='Taiwanese' hidden='false' />
             <CuisineChoice text='Chinese' hidden='false' />
           </View>
 
-          <View style={{ flex: 1, backgroundColor: 'grey', flexDirection: 'column' }}>
+          <View style={styles.vertColRightside}>
             <CuisineChoice text='Japanese' hidden='false'/>
             <CuisineChoice text='Korean' hidden='false' />
           </View>
@@ -196,12 +200,12 @@ class Rightside extends React.Component {
     else if (this.props.item == "European") {
       return (
         <View style={{ flex: 2, backgroundColor: 'skyblue', flexDirection: 'row' }}>
-          <View style={{ flex: 1, backgroundColor: 'grey', flexDirection: 'column' }}>
+          <View style={styles.vertColRightside}>
             <CuisineChoice text='British' hidden='false'  />
             <CuisineChoice text='Greek' hidden='false' />
           </View>
 
-          <View style={{ flex: 1, backgroundColor: 'grey', flexDirection: 'column' }}>
+          <View style={styles.vertColRightside}>
             <CuisineChoice text='French' hidden='false' />
             <CuisineChoice text='Mediterranean' hidden='false' />
           </View>
@@ -211,12 +215,12 @@ class Rightside extends React.Component {
     else if (this.props.item == "South East Asian") {
       return (
         <View style={{ flex: 2, backgroundColor: 'skyblue', flexDirection: 'row' }}>
-          <View style={{ flex: 1, backgroundColor: 'grey', flexDirection: 'column' }}>
+          <View style={styles.vertColRightside}>
             <CuisineChoice text='Vietnamese' hidden='false' />
             <CuisineChoice text='Filipino' hidden='false' />
           </View>
 
-          <View style={{ flex: 1, backgroundColor: 'grey', flexDirection: 'column' }}>
+          <View style={styles.vertColRightside}>
             <CuisineChoice text='Thai' hidden='false' />
             <CuisineChoice text='' hidden='true' />
           </View>
@@ -226,12 +230,12 @@ class Rightside extends React.Component {
     else if (this.props.item == "Other") {
       return (
         <View style={{ flex: 2, backgroundColor: 'skyblue', flexDirection: 'row' }}>
-          <View style={{ flex: 1, backgroundColor: 'grey', flexDirection: 'column' }}>
+          <View style={styles.vertColRightside}>
             <CuisineChoice text='Fast Food' hidden='false' />
             <CuisineChoice text='Mexican' hidden='false' />
           </View>
 
-          <View style={{ flex: 1, backgroundColor: 'grey', flexDirection: 'column' }}>
+          <View style={styles.vertColRightside}>
             <CuisineChoice text='Indian' hidden='false' />
             <CuisineChoice text='Middle Eastern' hidden='true' />
           </View>
@@ -381,5 +385,31 @@ const styles = StyleSheet.create({
 
   whiteBg: {
     backgroundColor: 'white',
+  },
+
+  //styling for columns and rows
+  vertColRightside: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    //set hidden to transparent
+    flexDirection: 'column',
+  },
+
+  //button style
+  buttonCategory: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRightWidth: 1,
+    borderColor: 'black'
+  },
+
+  buttonTypeOfFood: {
+    width: 100,
+    height: 100,
+    margin: 10,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 });
