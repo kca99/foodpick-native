b3ba9102c8c8fef68d53853be543b31569e870e6
Fluffisaurus <angushon96@gmail.com>
2018-08-22 18:13:26 -0700

    added: renderArray, combinedReducer,  add selected cuisine 2 outputArray

--- App.js
+++ App.js
@@ -3,28 +3,59 @@ import { TouchableHighlight, Button, View, Text, StyleSheet } from 'react-native
 import { createStackNavigator } from 'react-navigation';
 import styles from './style';
 import { createStore } from 'redux';
+import { combineReducers } from 'redux';
+
+const renderArray = [
+  [0           ,1               ,2            ,3               ],
+  ['Taiwanese' ,'British'       ,'Vietnamese' ,'Fast Food'     ],
+  ['Chinese'   ,'Greek'         ,'Filipino'   ,'Mexican'       ],
+  ['Japanese'  ,'French'        ,'Thai'       ,'Indian'        ],
+  ['Korean'    ,'Mediterranean' ,' '          ,'Middle Eastern']
+];
 
 const outputArray = [];
 
-function addCuisine(text){
+const addCuisine = (text) =>{
   return{ type: ADD_CUISINE, text }
 }
 
-function removeCuisine(text){
+const removeCuisine = (text) =>{
   return{ type: REMOVE_CUISINE, text }
 }
 
-const cuisineChoice = (state = 'empty', action) => {
+const addSelectedCuisine = (outputArray, text) =>{
+  //concat 
+  return [...outputArray, text];
+};
+
+// const removeSelectedCuisine = (outputArray, text) =>{
+
+// };
+
+const visibilityReducer = (state = 0, action) => {
+  switch(action.type){
+    case 'CHANGE_TYPE':
+      // not sure what to return, needs work
+      return state;
+    default:
+      return state;
+  }
+}
+
+//need to work on returns for each case, not sure if this is how we should call it
+const selectedReducer = (state = 'empty', action) => {
   switch(action.type) { 
     case 'ADD_CUISINE':
-      return state + 'text';
+      return addSelectedCuisine(outputArray, action.text);
     case 'REMOVE_CUISINE':
       return state - 'text';
     default:
       return state;
   }
 }
-const store = createStore(cuisineChoice);
+
+baseReducer = combineReducers({visibilityReducer, selectedReducer});
+const store = createStore(baseReducer);
 
 class HomeScreen extends React.Component {
   constructor(props) {
