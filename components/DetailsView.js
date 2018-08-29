f680f3cf428bb0e53c8379b5cf747047ced75fa5
Kevin Chung <kevinchungw@gmail.com>
2018-08-29 13:10:49 -0700

    Location retrieval

--- components/DetailsView.js
+++ components/DetailsView.js
@@ -1,8 +1,37 @@
 import React from 'react';
-import { TouchableHighlight, Button, View, Text, StyleSheet } from 'react-native';
-import { createStore, combineReducers } from 'redux';
+import { Platform, TouchableHighlight, Button, View, Text, StyleSheet } from 'react-native';
+import {Constants, Location, Permissions} from 'expo';
 
 class DetailsScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: null,
+      errorMessage: null,
+    }
+  }
+
+  componentWillMount(){
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this.getLocation();
+    }
+  }
+
+  getLocation= async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+  };
 
   render() {
     const { navigation } = this.props;
@@ -21,6 +50,12 @@ class DetailsScreen extends React.Component {
             />
             <Text> You have randomed: </Text>
             <Text> {items[index]}</Text>
+            <Text> {this.state.errorMessage}</Text>
+            <Text> {JSON.stringify(this.state.location)}</Text>
+            <Button
+              title="Redo location"
+              onPress={() => this.getLocation()}
+            />
           </View>
         </View>
       </View>
