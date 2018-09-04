2827a7d20626cfdb26d16f71c4483ef0780ff25c
Kevin Chung <kevinchungw@gmail.com>
2018-09-04 14:18:18 -0700

    Fixed Location handling

--- components/DetailsView.js
+++ components/DetailsView.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Platform, TouchableHighlight, Button, View, Text, StyleSheet } from 'react-native';
-import { Constants, Location, Permissions} from 'expo';
+import { Constants, Location, Permissions } from 'expo';
 import { MapView } from 'expo';
 
 
@@ -9,13 +9,13 @@ class DetailsScreen extends React.Component {
     super(props);
     this.state = {
       location: null,
-      errorMessage: null,
+      errorMessage: 'Waiting',
       longitude: null,
       latitude: null,
     }
   }
 
-  componentWillMount(){
+  componentWillMount() {
     if (Platform.OS === 'android' && !Constants.isDevice) {
       this.setState({
         errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
@@ -25,55 +25,62 @@ class DetailsScreen extends React.Component {
     }
   }
 
-  getLocation= async () => {
+  getLocation = async () => {
+    //Ask permission to retrieve location
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status !== 'granted') {
       this.setState({
         errorMessage: 'Permission to access location was denied',
       });
-      
+
     }
-    //this if statement below doesn't work properly, the below if condition is undefined zzz 
-    console.log(Expo.Location.getProviderStatusAsync())
-    if(Expo.Location.getProviderStatusAsync().locationServicesEnabled === false){
-      this.setState({
-        errorMessage: 'Please turn on location',
-      });
+    else { //permission granted YES
+      //this if statement below doesn't work properly, the below if condition is undefined zzz 
+      //Check if location is actually on
+      var provObj = await Expo.Location.getProviderStatusAsync();
+      console.log("provObj: ", provObj['gpsAvailable'])
+      if (provObj['gpsAvailable'] === false) { //Location is off you nitwits!
+        this.setState({
+          errorMessage: 'Please turn on location',
+        });
+      }
+      else{
+        let location = await Location.getCurrentPositionAsync({});
+        this.setState({ location: location });
+        this.setState({
+          longitude: this.state.location['coords']['longitude'],
+          latitude: this.state.location['coords']['latitude']
+        });
+        console.log(this.state.location['timestamp'])
+        // console.log(this.state.latitude)
+        // console.log(this.state.longitude)  
+      }
     }
-    let location = await Location.getCurrentPositionAsync({});
-    this.setState({ location: location });
-    this.setState({ 
-      longitude: this.state.location['coords']['longitude'],
-      latitude: this.state.location['coords']['latitude']
-     });
-    console.log(this.state.location['timestamp'])
-    // console.log(this.state.latitude)
-    // console.log(this.state.longitude)
-    
+
 
   };
-  
+
   render() {
     const { navigation } = this.props;
     const items = navigation.getParam('items');
     // console.log(items);
     // console.log(items[0])
-    const length=items.length;
-    var index = Math.floor((Math.random() * length ) );
+    const length = items.length;
+    var index = Math.floor((Math.random() * length));
     var latitude = this.state.latitude;
     var longitude = this.state.longitude;
-    
-    console.log(latitude , longitude);
 
-    if(latitude === null || longitude === null){
+    console.log(latitude, longitude);
+
+    if (latitude === null || longitude === null) {
       console.log("Rendering Waiting Page");
       return (
         <View>
-          <Text>Waiting</Text>
+          <Text>{this.state.errorMessage}</Text>
         </View>
       );
     }
-    else{
+    else {
       return (
         <View style={{ flex: 1 }}>
           <View style={{ flex: 1 }}>
@@ -90,22 +97,22 @@ class DetailsScreen extends React.Component {
                 title="Redo location"
                 onPress={() => this.getLocation()}
               />
-              
-             <MapView
-          style={{ flex: 1 }}
-          initialRegion={{
-            latitude: this.state.latitude,
-            longitude: this.state.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
+
+              <MapView
+                style={{ flex: 1 }}
+                initialRegion={{
+                  latitude: this.state.latitude,
+                  longitude: this.state.longitude,
+                  latitudeDelta: 0.0922,
+                  longitudeDelta: 0.0421,
+                }}
+              />
             </View>
           </View>
         </View>
       );
     }
-    
+
   }
 };
 
